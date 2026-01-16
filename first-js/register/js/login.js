b778d6bd0d4dccf71b3cb803f454c91e13046ff9
sephguirren <143795885+sephguirren@users.noreply.github.com>
2026-01-16 11:36:19 +0800

    Initial Commit

--- first-js/register/js/login.js
+++ first-js/register/js/login.js
@@ -1,14 +1,15 @@
-function login() {
+function register() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     const conpass = document.getElementById('conpass').value;
 
-    if(username.length === 0 || password.length === 0 || conpass.length === 0) {
-        alert("Username and Password is required");
-    } else if (password === conpass) {
-        alert('Karegister kan, Boi!');
-    } else {
-        alert('Madi ka makastrek, Boi!');
+    if (username.length === 0 || password.length === 0 || conpass.length === 0) {
+        alert("All fields are required.");
+    } 
+    else if (password !== conpass) {
+        alert("Passwords do not match.");
+    } 
+    else {
+        alert("Registration successful!");
     }
-    
 }
