52caa70e93203e9a159a48138816c1fa4a879be8
sephguirren <143795885+sephguirren@users.noreply.github.com>
2026-01-19 14:44:33 +0800

    Initial Commit

--- first-js/js/login.js
+++ first-js/js/login.js
@@ -1,13 +1,22 @@
-function login() {
+function register() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
+    const conpass = document.getElementById('conpass').value;
 
-    if(username.length === 0 || password.length === 0) {
-        alert("Username and Password is required");
-    } else if (username === 'admin' && password === 'password') {
-        alert('Wenla, Mabalin mo iaccess daytoy site!');
-    } else {
-        alert('Madi ka makastre, Boi!');
+    const error = document.getElementById('error');
+    const success = document.getElementById('success');
+
+    if (username.length === 0 || password.length === 0 || conpass.length === 0) {
+        error.innerHTML = "Username and Password fields cannot be empty.";
+        error.style.visibility = "visible";
+    } 
+    else if (password !== conpass) {
+        error.innerHTML = "Passwords do not match.";
+        error.style.visibility = "visible";
+    } 
+    else {
+        error.style.visibility = "hidden";
+        success.innerHTML = "Registration Successful!";
+        success.style.visibility = "visible";
     }
-    
 }
