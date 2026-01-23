20dd8180b79f6bff974d1703a5b78990973929da
sephguirren <143795885+sephguirren@users.noreply.github.com>
2026-01-23 11:50:02 +0800

    Update js

--- functions/js/calfunc.js
+++ functions/js/calfunc.js
@@ -1,1 +1,25 @@
-//calculator functions
+let display = document.getElementById("display");
+
+function num(val) {
+    display.value += val;
+}
+
+function oper(val) {
+    display.value += val;
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
+
+function ac() {
+    display.value = "";
+}
+
+function del() {
+    display.value = display.value.slice(0, -1);
+}
