301275863a60ed46b3b4545e3fced790e38a3fa8
sephguirren <143795885+sephguirren@users.noreply.github.com>
2026-01-23 11:03:23 +0800

    Initial Commit

---
+++ functions/js/functions.js
@@ -0,0 +1,13 @@
+function normal() {
+    const sum = getSum(5, 10);
+    alert (sum)
+}
+
+function withParameters(fname, lname) {
+    alert("Hello " + fname + " " + lname);
+}
+
+function getSum(num1, num2) {
+    const sum = num1 + num2;
+    return sum;
+}
