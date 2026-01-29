bca845caeb68dbecf5af4c2a985571ac424fd9c4
sephguirren <143795885+sephguirren@users.noreply.github.com>
2026-01-29 17:50:11 +0800

    Initial Commit

--- functions/js/calfunc.js
+++ functions/js/calfunc.js
@@ -1,25 +1,46 @@
 let display = document.getElementById("display");
+let operatordisplay = document.getElementById("operator");
+let savednum = "";
 
 function num(val) {
-    display.value += val;
-}
-
-function oper(val) {
-    display.value += val;
-}
-
-function calculate() {
-    try {
-        display.value = eval(display.value);
-    } catch (error) {
-        display.value = "Error";
+    if (display.value === "0") {
+        display.value = val;
+    } else {
+        display.value += val;
     }
 }
 
+function oper(op) {
+    savednum = display.value;
+    operatordisplay.value = op;
+    display.value = "0";
+}
+
+function calculate() {
+    let secondnum = display.value;
+    let op = operatordisplay.value;
+    let output = savednum + op + secondnum;
+
+    try {
+        display.value = eval(output);
+    } catch (error) {
+        display.value = "Error";
+    }
+
+    operatordisplay.value = "";
+    savednum = "";
+}
+
 function ac() {
-    display.value = "";
+    display.value = "0";
+    operatordisplay.value = "";
+    savednum = "";
 }
 
 function del() {
-    display.value = display.value.slice(0, -1);
+    if (display.value.length > 1) {
+        display.value = display.value.slice(0, -1);
+    } else {
+        display.value = "0";
+    }
 }
